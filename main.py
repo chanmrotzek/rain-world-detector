340d8e8ae6fe5607b684d47908f9eb257b61e1d0
chanmrotzek <84637553+nuken-official@users.noreply.github.com>
2023-04-06 14:29:53 -0700

    please

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ intents.messages = True
 intents.reactions = True
 intents.guilds = True
 
-discord_user = int(os.environ['USER_ID'])
+discord_user = os.environ['USER_ID']
 
 client = discord.Client(intents=intents)
 
@@ -14,23 +14,24 @@ rain_world_counter = 0  # initialize the counter
 
 @client.event
 async def on_ready():
-    print(discord_user)
     print(f'We have logged in as {client.user}')
 
 @client.event
 async def on_message(message):
-    print(f'I am a stupid Discord bot! A message! Wow!')
-    global rain_world_counter  # declare the counter as global
-
+    print('Received message:', message.content)
+    
     if message.author == client.user:
         return
 
-    if "rain world" in message.content.lower():
+    if message.author.id == discord_user and "rain world" in message.content.lower():
+        print('Recognized command:', message.content)
         rain_world_counter += 1  # increment the counter
         await message.add_reaction("🚩")  # react with an emoji
 
     if message.content == "!rainworldcount":
+        print('Recognized command:', message.content)
         await message.channel.send(f"Käselord has mentioned Rain World {rain_world_counter} times. I'll continue to monitor their usage.")  # return the counter's value
+
 
 token = os.environ['DISCORD_TOKEN']
 
