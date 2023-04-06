92c99ca3fd305ebd785b9be0ce69df3ae17d372e
chanmrotzek <84637553+nuken-official@users.noreply.github.com>
2023-04-06 13:43:55 -0700

    lmao

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import discord
+import os
 
 intents = discord.Intents.default()
 intents.messages = True
+
+discord_user = os.getenv('USER_ID')
 
 client = discord.Client(intents=intents)
 
@@ -18,14 +21,13 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.author.id == 750845810396364900 and "rain world" in message.content.lower():
+    if message.author.id ==  and "rain world" in message.content.lower():
         rain_world_counter += 1  # increment the counter
         await message.add_reaction("🚩")  # react with an emoji
 
     if message.content == "!rainworldcount":
         await message.channel.send(f"Käselord has mentioned Rain World {rain_world_counter} times. I'll continue to monitor their usage.")  # return the counter's value
 
-token = "MTA5MzYxODE5MTM3NDIyOTUzNQ.Gg4yjn.Rb_Rc5SgL4dfEWh-zBjxY5s2bwQmpKbzVZePDQ"
-print(f"Using token: {token}")
+token = os.getenv('DISCORD_TOKEN')
 
 client.run(token)
